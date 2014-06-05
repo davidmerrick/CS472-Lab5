23ef2304f4c72218e77df94716f3be40de08131f
David Merrick <david@david-merrick.com>
2014-06-05 02:25:11 -0700

    Got part 2 working

--- part2.c
+++ part2.c
@@ -1,4 +1,5 @@
-nclude <stdio.h>
+#include <endian.h>
+#include <stdio.h>
 
 //General idea:
 // BIG_ENDIAN is defined in endian.h as the byte order. It differs between systems.
@@ -14,8 +15,7 @@ nclude <stdio.h>
 
 //BIG_ENDIAN on my system: 4321
 
-#define BYTE_SWAP(bytes) (bytes << ((4*(BIG_ENDIAN/1000)-4) & 0xF000)) | (bytes << (((4*(BIG_ENDIAN % 1000 / 100)-4-4)) & 0x0F00)) & 0xFF00
-//| (bytes >> ((4*(BIG_ENDIAN % 100 / 10)-4) & 0x00F0)) | (bytes >> ((4*(BIG_ENDIAN % 100 / 10) + 4) & 0x000F))
+#define BYTE_SWAP(bytes) (bytes << (4*(BIG_ENDIAN/1000)-4) & 0xF000) | (bytes << (4*(BIG_ENDIAN % 1000 / 100)-4*2) & 0x0F00) | (bytes >> abs(4*(BIG_ENDIAN % 1000 / 100)-4*3-4) & 0x00F0) | (bytes >> abs(4*(BIG_ENDIAN % 100 / 10)-4*4-4) & 0x000F)
 int main(int argc, char **argv)
 {
 	short val;
@@ -25,28 +25,7 @@ int main(int argc, char **argv)
 	p_val[0] = 0x12;
 	p_val[1] = 0x34;
 	
-	
-	int first = 4*(BIG_ENDIAN/1000)-4;
-	int second = 4*(BIG_ENDIAN % 1000 / 100)-4*2;
-
-	short bytes = 0x1234;
-	short firstShift = (bytes << first & 0xF000);
-	short secondShift = (bytes << second & 0x0F00);
-	
-	
-	int third = abs(4*(BIG_ENDIAN/1000)-4*3);
-	int fourth = abs(4*(BIG_ENDIAN % 1000 / 100)-4*4-8);
-	
-	short thirdShift = (bytes >> third & 0x00F0);
-	short fourthShift = (bytes >> fourth & 0x000F);
-
-	short testShift1 = firstShift | secondShift | thirdShift | fourthShift;
-	short testShift2 = firstShift | secondShift | thirdShift | fourthShift;
-	
-	printf("%x\n", testShift1);
-	printf("%x\n", testShift2);
-	
-	val = BYTE_SWAP(val);
+	val = BYTE_SWAP(0x1234);
 	
 	printf("%x\n", val);
 	
